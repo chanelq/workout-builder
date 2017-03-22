dd0b859ae244a2f681273690effdbc611ba71087
Chanel Quisol <chanel.quisol@gmail.com>
2017-03-22 07:27:41 -0700

    added workout class

--- ng-app/app.component.ts
+++ ng-app/app.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+export class Workout {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{workout.name}} details!</h2>
+    <div><label>id: </label>{{workout.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="workout.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Workout Builder';
+  workout: Workout = {
+    id: 1,
+    name: 'Half Marathon'
+  };
+}
