c789578f19ab940ccf255c500d1a286326014136
Chanel Quisol <chanel.quisol@gmail.com>
2017-04-12 08:05:33 -0700

    index displays list of workouts + details when name is clicked

---
+++ ng-app/workout-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+import { Workout } from './workout';
+@Component({
+  selector: 'workout-detail',
+  template: `
+    <div *ngIf="workout">
+      <h2>{{workout.name}} details!</h2>
+      <div><label>id: </label>{{workout.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="workout.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+export class WorkoutDetailComponent {
+  @Input() workout: Workout;
+}
--- ng-app/workout.ts
+++ ng-app/workout.ts
@@ -1,11 +1,4 @@
 export class Workout {
   id: number;
   name: string;
-  exerciseype:
-  description
-  sets
-  reps
-  rest
-  duration
-  distance
 }
